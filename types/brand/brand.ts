998cac84aa797dcc44a22f7a3ceb59566fc482ac
Hongchray <songhongchray@gmail.com>
2025-08-15 11:44:30 +0700

    remove space

--- types/brand/brand.ts
+++ types/brand/brand.ts
@@ -1,30 +1,29 @@
 export interface Brand {
-  id: string
-  name: string
-  slug: string
-  description: string
-  logo_url: string
-  is_featured: number
-  created_at: string
-  updated_at: string
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  logo_url: string;
+  is_featured: number;
+  created_at: string;
+  updated_at: string;
 }
 
 export interface BrandResponse {
-  status: number
-  message: string
-  data: Brand[]
+  status: number;
+  message: string;
+  data: Brand[];
 }
-
 export interface BrandFormState {
-  name: string
-  slug: string
-  description: string
-  logo_url: string
-  is_featured: boolean
+  name: string;
+  slug: string;
+  description: string;
+  logo_url: string;
+  is_featured: boolean;
 }
 
 export interface ApiResponse {
-  status?: number
-  message: string
-  data?: any
+  status?: number;
+  message: string;
+  data?: any;
 }
